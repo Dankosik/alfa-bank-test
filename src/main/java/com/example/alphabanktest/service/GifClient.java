7e6440f76edf6404c27e84c57c29358362322207
dankos <daniil.koryto@gmail.com>
2021-09-24 02:21:57 +1000

    added gif feign client

---
+++ src/main/java/com/example/alphabanktest/service/GifClient.java
@@ -0,0 +1,12 @@
+package com.example.alphabanktest.service;
+
+import com.example.alphabanktest.dto.gif.GifApiWrapper;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "giphy", url = "https://api.giphy.com/v1/gifs/search?api_key=MoHieYC44O2SRXtBfEtumvod6oCwER56")
+public interface GifClient {
+    @GetMapping
+    GifApiWrapper getGifUrlByName(@RequestParam String q);
+}
