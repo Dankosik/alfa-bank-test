d6c5ff5933d053b829b3db8fbdc4c574c3acc834
dankos <daniil.koryto@gmail.com>
2021-09-24 02:20:35 +1000

    added fields

--- src/main/java/com/example/alphabanktest/dto/ExchangeRate.java
+++ src/main/java/com/example/alphabanktest/dto/ExchangeRate.java
@@ -1,2 +1,13 @@
-package com.example.alphabanktest.dto;public class ExchangeRate {
+package com.example.alphabanktest.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+
+@Data
+@NoArgsConstructor
+public class ExchangeRate {
+    private String base;
+    private HashMap<String, Double> rates;
 }
