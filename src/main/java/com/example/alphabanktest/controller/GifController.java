1504f659f9ec9d3732e1f2da01558a87638af338
dankos <daniil.koryto@gmail.com>
2021-09-24 02:35:42 +1000

    created GifController

--- src/main/java/com/example/alphabanktest/controller/GifController.java
+++ src/main/java/com/example/alphabanktest/controller/GifController.java
@@ -1,2 +1,25 @@
-package com.example.alphabanktest.controller;public class GifController {
+package com.example.alphabanktest.controller;
+
+import com.example.alphabanktest.dto.gif.GifApiOriginalWrapper;
+import com.example.alphabanktest.service.GifClient;
+import com.example.alphabanktest.service.GifService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/gif")
+@AllArgsConstructor
+public class GifController {
+    private final GifClient gifClient;
+    private final GifService gifService;
+
+    @GetMapping("/{key-word}")
+    public GifApiOriginalWrapper getGifUrl(@PathVariable("key-word") String keyWord) {
+        GifApiOriginalWrapper response = new GifApiOriginalWrapper();
+        response.setUrl(gifService.getRandomGifUrl(gifClient.getGifUrlByName(keyWord)));
+        return response;
+    }
 }
