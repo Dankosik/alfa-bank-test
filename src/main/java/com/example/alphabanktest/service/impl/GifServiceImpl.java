a600e7d3102e8359ef135996547dce433575c32c
dankos <daniil.koryto@gmail.com>
2021-09-24 17:55:42 +1000

    change implementation

--- src/main/java/com/example/alphabanktest/service/impl/GifServiceImpl.java
+++ src/main/java/com/example/alphabanktest/service/impl/GifServiceImpl.java
@@ -1,17 +1,24 @@
 package com.example.alphabanktest.service.impl;
 
+import com.example.alphabanktest.dto.gif.GifApiOriginalWrapper;
 import com.example.alphabanktest.dto.gif.GifApiWrapper;
+import com.example.alphabanktest.feign.GifClient;
 import com.example.alphabanktest.service.GifService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Service
+@AllArgsConstructor
 public class GifServiceImpl implements GifService {
+    private final GifClient gifClient;
+
     @Override
-    public String getRandomGifUrl(GifApiWrapper gifApiWrapper) {
+    public GifApiOriginalWrapper getRandomGifByName(String name) {
+        GifApiWrapper gifApiWrapper = gifClient.getGifApiWrapperByName(name);
         int randomIndex = getRandomIndex(gifApiWrapper);
-        return gifApiWrapper.getData()[randomIndex].getImages().getOriginal().getUrl();
+        return gifApiWrapper.getData()[randomIndex].getImages().getOriginal();
     }
 
     private int getRandomIndex(GifApiWrapper gifApiWrapper) {
