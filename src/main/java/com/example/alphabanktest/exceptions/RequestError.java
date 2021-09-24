d2adb2009314f4b67df0fd71ad9b67bc16d37833
dankos <daniil.koryto@gmail.com>
2021-09-25 01:07:11 +1000

    added RequestError

---
+++ src/main/java/com/example/alphabanktest/exceptions/RequestError.java
@@ -0,0 +1,16 @@
+package com.example.alphabanktest.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class RequestError {
+    private final HttpStatus error;
+    private final String message;
+    private Date timestamp;
+    private int status;
+}
