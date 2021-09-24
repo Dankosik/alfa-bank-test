519cce2a70cc5bc4bb9d5d7dd1f5993e34370981
dankos <daniil.koryto@gmail.com>
2021-09-24 23:04:46 +1000

    changed api environment

--- src/main/java/com/example/alphabanktest/feign/ExchangeRateClient.java
+++ src/main/java/com/example/alphabanktest/feign/ExchangeRateClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "exchange-rate", url = "${exchange-rate-api.url}")
+@FeignClient(name = "exchange-rate", url = "${exchange-rate-api.url.value}")
 public interface ExchangeRateClient {
     @GetMapping("/latest.json?app_id=${exchange-rate-api.api-key}")
     ExchangeRate getLatestExchangeRate();
--- src/main/java/com/example/alphabanktest/feign/GifClient.java
+++ src/main/java/com/example/alphabanktest/feign/GifClient.java
@@ -5,8 +5,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "giphy", url = "${gif-api.url}")
+@FeignClient(name = "giphy", url = "${gif-api.url.value}")
 public interface GifClient {
-    @GetMapping("/search?api_key=${gif-api.api-key}")
+    @GetMapping("/search?api_key=${gif-api.api-key}" +
+            "&limit=${gif-api.url.limit-argument}" +
+            "&offset=${gif-api.url.offset-argument}" +
+            "&rating=${gif-api.url.rating-argument}" +
+            "&lang=${gif-api.url.lang-argument}")
     GifApiWrapper getGifApiWrapperByName(@RequestParam String q);
 }
