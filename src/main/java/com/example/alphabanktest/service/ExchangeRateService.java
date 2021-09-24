ab0d36abe08f6c370438d82a3e7fd8d5468ef56f
dankos <daniil.koryto@gmail.com>
2021-09-25 01:07:57 +1000

    changed public methods to private

--- src/main/java/com/example/alphabanktest/service/ExchangeRateService.java
+++ src/main/java/com/example/alphabanktest/service/ExchangeRateService.java
@@ -1,9 +1,5 @@
 package com.example.alphabanktest.service;
 
 public interface ExchangeRateService {
-    Double getLatestCurrencyValueVersusDollar(String currency);
-
-    Double getYesterdayCurrencyValueVersusDollar(String currency);
-
     boolean isLatestCurrencyValueMoreThanYesterday(String currency);
 }
--- src/main/java/com/example/alphabanktest/service/impl/ExchangeRateServiceImpl.java
+++ src/main/java/com/example/alphabanktest/service/impl/ExchangeRateServiceImpl.java
@@ -1,35 +1,48 @@
 package com.example.alphabanktest.service.impl;
 
+import com.example.alphabanktest.exceptions.CurrencyNotFoundException;
 import com.example.alphabanktest.feign.ExchangeRateClient;
 import com.example.alphabanktest.service.ExchangeRateService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 
 @Service
+@Slf4j
 @AllArgsConstructor
 public class ExchangeRateServiceImpl implements ExchangeRateService {
     private final ExchangeRateClient exchangeRateClient;
 
     @Override
-    public Double getLatestCurrencyValueVersusDollar(String currency) {
-        return exchangeRateClient.getLatestExchangeRate().getRates().get(currency);
+    public boolean isLatestCurrencyValueMoreThanYesterday(String currency) {
+        return this.getLatestCurrencyValueVersusDollar(currency) > this.getYesterdayCurrencyValueVersusDollar(currency);
     }
 
-    @Override
-    public Double getYesterdayCurrencyValueVersusDollar(String currency) {
+    private Double getLatestCurrencyValueVersusDollar(String currency) {
+        HashMap<String, Double> rates = exchangeRateClient.getLatestExchangeRate().getRates();
+        if (!rates.containsKey(currency)) {
+            log.warn("Currency: " + currency + " is not found");
+            throw new CurrencyNotFoundException("Currency: " + currency + " is not found");
+        }
+        return rates.get(currency);
+    }
+
+    private Double getYesterdayCurrencyValueVersusDollar(String currency) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_MONTH, -1);
-        return exchangeRateClient.getExchangeRateByDate(simpleDateFormat.format(calendar.getTime()))
-                .getRates()
-                .get(currency);
-    }
 
-    @Override
-    public boolean isLatestCurrencyValueMoreThanYesterday(String currency) {
-        return this.getLatestCurrencyValueVersusDollar(currency) > this.getYesterdayCurrencyValueVersusDollar(currency);
+        HashMap<String, Double> rates = exchangeRateClient
+                .getExchangeRateByDate(simpleDateFormat.format(calendar.getTime())).getRates();
+
+        if (!rates.containsKey(currency)) {
+            log.warn("Currency: " + currency + " is not found");
+            throw new CurrencyNotFoundException("Currency: " + currency + " is not found");
+        }
+        return rates.get(currency);
     }
 }
